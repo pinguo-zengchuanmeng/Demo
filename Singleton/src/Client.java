ef426fad6559292a553719b54dc9c9a0a416834f
zengchuanmeng <zengchuanmeng@camera360.com>
2017-09-02 10:31:35 +0800

    [曾传猛] 1.修改工程名称

---
+++ Singleton/src/Client.java
@@ -0,0 +1,8 @@
+/**
+ * Created by zengchuanmeng on 2017/8/9.
+ */
+public class Client {
+    public static void main(String args[]) {
+        Singleton.get().sayHello();
+    }
+}
--- Singleton/src/Singleton.java
+++ Singleton/src/Singleton.java
@@ -1,7 +1,11 @@
+import java.util.Random;
+
 /**
  * Created by zengchuanmeng on 2017/8/8.
  */
 public class Singleton {
+
+    private int i = new Random(5);
     private static Singleton instance = new Singleton();
 
     private Singleton() {
@@ -10,5 +14,9 @@ public class Singleton {
 
     public static Singleton get() {
         return instance;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello everyone");
     }
 }
